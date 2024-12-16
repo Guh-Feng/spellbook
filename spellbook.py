ca82b8ec419c86552777bec59a32cd6e9f2015ba
Guh Feng <guh.feng@gmail.com>
2024-12-16 02:13:03 -0500

    Size and Positioning, Handling Special Key on Press, Tab as Back

--- spellbook.py
+++ spellbook.py
@@ -7,7 +7,7 @@ from time import sleep
 import os
 
 # Learn about dev chrome control, more general creation of folders and applying scripts in files with routing logic or manual folder creation with auto script finding
-
+# Outside scripts return 0, configures scripts to run, have the window work and scripts run when not withdrawn
 
 
 ########## Tkinter Initialization Start ##########
@@ -17,10 +17,10 @@ root = Tk()
 root.title("Spellbook")
 root.attributes("-topmost", 1)
 root.overrideredirect(True)
-root.geometry('600x200+700-49')
+root.geometry('600x150+700-49')
 
 #Mainframe settings
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = ttk.Frame(root, padding="12 12 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -54,7 +54,7 @@ def toggle_menu():
         root.withdraw()
         withDraw = False
     else:
-        root.wm_deiconify()
+        root.deiconify()
         withDraw = True
 
 def close_menu():
@@ -81,17 +81,30 @@ def on_press(key):
     global defaultKeys
     global menuLabel
 
-    defaultKeyIndex = defaultKeys.find('{0}'.format(key.char))
-    currentDirectory += '/' + os.listdir(currentDirectory)[defaultKeyIndex]
-    currentDirectories = os.listdir(currentDirectory)
-    dirLength = len(currentDirectories)
-    newMenu = ''
-    for i in range(dirLength):
-        newMenu += defaultKeys[i] + '. ' + currentDirectories[i] + '\n'
+    try:
+        defaultKeyIndex = defaultKeys.find('{0}'.format(key.char))
+        currentDirectory += '/' + os.listdir(currentDirectory)[defaultKeyIndex]
+        currentDirectories = os.listdir(currentDirectory)
+        dirLength = len(currentDirectories)
+        newMenu = ''
 
-    menuLabel.configure(text = newMenu)
-    print('{0}'.format(key.char))
-    print(newMenu)
+        for i in range(dirLength):
+            newMenu += defaultKeys[i] + '. ' + currentDirectories[i] + '\n'
+
+        menuLabel.configure(text = newMenu)
+
+    except AttributeError:
+        if(key == keyboard.Key.tab):
+            currentDirectory = currentDirectory[0:currentDirectory.rfind('/')]
+            currentDirectories = os.listdir(currentDirectory)
+            dirLength = len(currentDirectories)
+            newMenu = ''
+
+            for i in range(dirLength):
+                newMenu += defaultKeys[i] + '. ' + currentDirectories[i] + '\n'
+
+            menuLabel.configure(text = newMenu)
+   
 
 listener = keyboard.Listener(
     on_press=on_press)
